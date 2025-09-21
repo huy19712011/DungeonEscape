94b69cf0c08345655f5f8db3f296542da5661d6b
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-09-21 22:54:52 +0700

    v77: Setting Visibility

--- Source/DungeonEscape/Lock.cpp
+++ Source/DungeonEscape/Lock.cpp
@@ -26,6 +26,9 @@ ALock::ALock()
 void ALock::BeginPlay()
 {
 	Super::BeginPlay();
+
+	SetIsKeyPlaced(true);
+	// SetIsKeyPlaced(false);
 	
 }
 
@@ -35,4 +38,20 @@ void ALock::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void ALock::SetIsKeyPlaced(bool NewIsKeyPlaced)
+{
+	IsKeyPlaced = NewIsKeyPlaced;
+
+	TriggerComp->Trigger(NewIsKeyPlaced);
+	KeyItemMesh->SetVisibility(NewIsKeyPlaced);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+bool ALock::GetIsKeyPlaced()
+{
+	return IsKeyPlaced;
+}
+
 
